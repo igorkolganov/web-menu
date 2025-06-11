a08b48a4445aac85bb29bb32b651050844db169a
Igor Kolganov <igor3igor@rambler.ru>
2025-06-11 05:50:25 -0400

    frontend config add constant to hide menu or not

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,3 @@
 const MENU_LOCATION = 'topLeft' // Possible values: topLeft, topRight, bottomLeft, bottonRight
 const MENU_APPEARANCE = 'hided' // Possible values: hided, shown
+const MENU_HIDE_AFTER_CHOSSE = 'yes' // Possible values: yes, no
